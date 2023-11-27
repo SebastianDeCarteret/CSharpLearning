249e6329464927bcdde18b3f0449c37d7063b359
Seb <sebastian.decarteret@barnett-waddingham.co.uk>
2023-11-27 16:11:13 +0000

    Add project files.

---
+++ EmployeeBadge.cs
@@ -0,0 +1,54 @@
+﻿using ConsoleTables;
+
+namespace EmployeeNames
+{
+    class EmployeeDetails
+    {
+        public string? name, job, email, phone;
+
+        public EmployeeDetails(string? name, string? job, string? email, string? phone)
+        {
+            this.name = name;
+            this.job = job;
+            this.email = email;
+            this.phone = phone;
+        }
+    }
+
+    internal class EmployeeBadge
+    {
+        public static EmployeeDetails? EmployeeDetailsInstance;
+
+        public static void GetSetDetials()
+        {
+            EmployeeDetailsInstance = new EmployeeDetails(null, null, null, null);
+            GetSetName();
+            GetSetJob();
+            GetSetEmail();
+            GetSetPhone();
+            var table = new ConsoleTable("name", "job", "email", "phone");
+            table.AddRow(EmployeeDetailsInstance.name, EmployeeDetailsInstance.job, EmployeeDetailsInstance.email, EmployeeDetailsInstance.phone);
+            table.Write();
+        }
+        private static void GetSetName()
+        {
+            Console.WriteLine("Input Name:");
+            EmployeeDetailsInstance.name = Console.ReadLine();
+        }
+        private static void GetSetJob()
+        {
+            Console.WriteLine("Input Job:");
+            EmployeeDetailsInstance.job = Console.ReadLine();
+        }
+        private static void GetSetEmail()
+        {
+            Console.WriteLine("Input Email:");
+            EmployeeDetailsInstance.email = Console.ReadLine();
+        }
+        private static void GetSetPhone()
+        {
+            Console.WriteLine("Input Phone:");
+            EmployeeDetailsInstance.phone = Console.ReadLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using EmployeeNames;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //WeatherUtilities.Report("London", WeatherUtilities.FahrenheitToCelsius(32), 65);
+            //WeatherUtilities.Report("Bristol", 15, 55);
+            //Console.WriteLine(WeatherUtilities.CelsiusToFahrenheit(32));
+            //Console.WriteLine("Input Name:");
+            //string? InputName= Console.ReadLine();
+            //Console.WriteLine($"Hello {InputName}!");
+            //Console.WriteLine(EmployeeBadge.GetName());
+            //Console.WriteLine(EmployeeBadge.GetJob());
+            //EmployeeBadge.GetName();
+            EmployeeBadge.GetSetDetials();
+
+        }
+    }
+}
